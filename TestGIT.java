f96905ac2139f97f0106ae0312516c0dca6ae695
bowkun <bowkun@wszib.edu.pl>
2019-11-23 16:17:36 +0100

    Initial commit

---
+++ TestGIT.java
@@ -0,0 +1,14 @@
+public class TestGIT {
+
+    public static void main(String[] args) {
+
+        int x = 2;
+
+
+        if(x == 2){
+            System.out.println("GitHub jest fajny");
+        }
+
+    }
+
+}
